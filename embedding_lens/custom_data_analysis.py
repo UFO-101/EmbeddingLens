7ca2eb7f1a64feedef190134bd1123165c97b2c0
UFO-101 <josephmiller101@gmail.com>
2024-04-14 11:55:40 +0000

    Fix custom_data_analysis.py

--- embedding_lens/custom_data_analysis.py
+++ embedding_lens/custom_data_analysis.py
@@ -1,8 +1,12 @@
 from typing import Final
+
+from spellchecker import SpellChecker
 from embedding_lens.sae import SparseAutoencoder
 from embedding_lens.utils import repo_path_to_abs_path
 from transformer_lens import HookedTransformer
 import torch as t
+
+spell = SpellChecker()
 
 MODEL_NAME: Final[str] = "gpt2"
 DEVICE = "cuda" if t.cuda.is_available() else "cpu"
@@ -13,6 +17,15 @@ N_FEATURES = 2000
 N_EPOCHS = 25000
 L1_LAMBDA = 2e-1
 LR = 1e-3
+
+# filter to english words
+tok_strs: List[str] = model.to_str_tokens(t.arange(model.cfg.d_vocab))  # type: ignore
+tok_strs = [word.strip() for word in tok_strs]
+correct_words = spell.known(tok_strs)
+en_idxs = [i for i, tok_str in enumerate(tok_strs) if tok_str.strip() in correct_words]
+print("embeds", embeds.shape)
+embeds = embeds[en_idxs]
+print("en embeds", embeds.shape)
 
 file_name = f"sae_{MODEL_NAME}_{N_FEATURES}_feats_{N_EPOCHS}_epochs_{L1_LAMBDA}_l1_{LR}_lr.pth"
 file_path = repo_path_to_abs_path(f"trained_saes/{file_name}")
